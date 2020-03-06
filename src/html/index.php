703e47875c0e5245632acab53ab20848a7198ff2
Aaron Traas <aaron@traas.org>
2020-03-06 12:18:13 -0500

    Now enabled/disabled based on setting

--- src/html/index.php
+++ src/html/index.php
@@ -5,23 +5,17 @@ error_reporting(E_ALL);
 
 require '../../config.inc.php';
 
-function auth_failed($message) {
-  header('WWW-Authenticate: Basic realm="send.traas.org"');
-  header('HTTP/1.0 401 Unauthorized');
-  die("<h1>HTTP/1.0 401 Unauthorized</h1><p>$message</p>");
-}
-
 function message_builder($message, $success) {
   $alert_type = ($success == true) ? 'success' : 'danger';
   return '<div class="row"><div class="col alert alert-' . $alert_type . '" role="alert">' . $message . '</div></div>';
 }
 
+if (ENABLED != true) {
+  die();
+}
+
 $message = '';
-if (!(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))) {
-  auth_failed('Authentication required');
-} else if (!(($_SERVER['PHP_AUTH_USER'] == HTTP_AUTH_USER) && ($_SERVER['PHP_AUTH_PW'] == HTTP_AUTH_PASSWORD))) {
-  auth_failed('Invalid username/password');
-} else if( $_POST && isset($_POST['bodyText']) ) {
+if( $_POST && isset($_POST['bodyText']) ) {
   require '../vendor/autoload.php';
 
   $ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
